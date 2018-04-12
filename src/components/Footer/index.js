6c7b2daf3a390e97baae943c3034e554a1728ad7
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-04-13 01:28:06 +0530

    Adding copyright footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,9 @@ import styles from "./index.css"
 
 const Footer = () => (
   <footer className={ styles.footer }>
-    { /* If you like Phenomic, this is a way to share the love ;) */ }
     <p>
-      <a
-        href={ process.env.PHENOMIC_HOMEPAGE }
-        className={ styles.phenomicReference }
-      >
-        { "Website generated with " }
-        <span className={ styles.phenomicReferenceName }>
-          {  `<${ process.env.PHENOMIC_NAME} />` }
-        </span>
+      <a href="https://gauravagarwalr.com" className={ styles.phenomicReference }>
+        Copyright © {(new Date()).getFullYear()} Gaurav Agarwal. All rights reserved.
       </a>
     </p>
   </footer>
