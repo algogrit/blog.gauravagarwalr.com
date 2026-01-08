5840ee12bb829d4dc78f078587f3c2af04445deb
Gaurav Agarwal <algogrit@gmail.com>
2026-01-08 11:22:02 +0530

    Including react & tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
